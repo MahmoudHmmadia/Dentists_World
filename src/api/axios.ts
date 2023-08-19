667d7545dbaaa5a85d499b24017663d2a6a81233
Mahmoud Hmmadia <102804429+MahmoudHmmadia@users.noreply.github.com>
2023-08-19 10:11:36 +0300

    fixing request

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 const SERVER_URL = "https://dentist-world-api.onrender.com";
 const local = "http://localhost:3500";
 const myAxios = axios.create({
-  baseURL: local,
+  baseURL: SERVER_URL,
 });
 export default myAxios;
